b0a8856011c076ee649f638b5bc2116e696bbb0a
Dilpreet <155355461+Dilpreet-Sandhu@users.noreply.github.com>
2024-10-14 12:39:42 +0530

    fixed token issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,28 @@
-import { getToken } from "next-auth/jwt";
+
 import { NextRequest, NextResponse } from "next/server";
+import {parse} from 'cookie'
 export { default } from 'next-auth/middleware';
 
 
 
 export async function middleware(request : NextRequest) {
 
-    const token = await getToken({req : request});
+;
+   const cookies = request.headers.get("cookie");
+   const parsedCookies = parse(cookies || "");
 
+   const authToken = parsedCookies['next-auth.session-token'] || parsedCookies['__Secure-next-auth.session-token'];
+   console.log("authToken" + authToken);
     const url = request.nextUrl;
 
-    if (token && (
+    if (authToken && (
         url.pathname.startsWith("/login") ||
         url.pathname.startsWith("/sign-up") || 
         url.pathname.startsWith("/")
     ) && !url.pathname.startsWith("/dashboard")) {
         return NextResponse.redirect(new URL("/dashboard",request.nextUrl))
     }
-    if (!token && (url.pathname.startsWith("/dashboard") || url.pathname.startsWith("/")) && !url.pathname.startsWith("/login") && !url.pathname.startsWith("/sign-up")) {
+    if (!authToken && (url.pathname.startsWith("/dashboard") || url.pathname.startsWith("/")) && !url.pathname.startsWith("/login") && !url.pathname.startsWith("/sign-up")) {
         return NextResponse.redirect(new URL("/login",request.nextUrl));
     }
 
